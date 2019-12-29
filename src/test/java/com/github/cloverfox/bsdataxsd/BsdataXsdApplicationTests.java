038b3fd7ce9ea468f55adc0e5281f8a7fda1710b
Clover Fox <cloverfox@protonmail.ch>
2019-12-29 20:25:42 +0000

    initial commit

---
+++ src/test/java/com/github/cloverfox/bsdataxsd/BsdataXsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cloverfox.bsdataxsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsdataXsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
